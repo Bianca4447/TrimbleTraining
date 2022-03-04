2692d0b240ec0770ce1c0019f8be76bf9312d5a4
Bianca <bia.marina96@yahoo.com>
2022-03-04 12:40:00 +0200

    Made Waterfall

--- AngularApp/notes-app/src/app/tema/tema.module.ts
+++ AngularApp/notes-app/src/app/tema/tema.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
+import { FirstComponent } from './module1/first/first.component';
+import { SecondComponent } from './module1/second/second.component';
 
 
 
 @NgModule({
   declarations: [
     Comp1Component,
-    Comp2Component
+    Comp2Component,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     Comp1Component,
-    Comp2Component
+    Comp2Component,
+    FirstComponent,
+    SecondComponent
   ]
 })
 export class TemaModule { }
