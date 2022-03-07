c8e02928ea1b3d03057bbaac96ab407fcd4bb589
Bianca <bia.marina96@yahoo.com>
2022-03-07 16:57:42 +0200

    Workshop 4

---
+++ notes-app/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoteComponent } from './note/note.component';
+import { ToolsComponent } from './tools/tools.component';
+import { TestComponent } from './test/test.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NoteComponent,
+    ToolsComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ notes-app/src/app/tools/tools.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tools',
+  templateUrl: './tools.component.html',
+  styleUrls: ['./tools.component.scss']
+})
+export class ToolsComponent implements OnInit {
+
+  title:string="Add note";
+  titleColor:string="red";
+  noteContent:string="";
+  colorContent:string = "";
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+  setTitle(){
+    this.noteContent = "Note content modificat de pe buton";
+  }
+
+  setColor(){
+    this.colorContent = "Color content"
+  }
+
+}
